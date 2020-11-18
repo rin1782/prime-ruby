774b40f0a20409fd1de9a1ac3895644a78e5c478
Erin Cola <colaerin@yahoo.com>
2020-11-18 15:16:02 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    else 
+        (2..(num - 1)).each do |i|
+            if (num % i) == 0
+                return false
+            end
+        end
+        return true
+    end
+end 
